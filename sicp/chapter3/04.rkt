de930b8445b689db96a4ea15801d126c16bab523
tre3p <whereareyo@yahoo.com>
2024-05-05 13:39:35 +0300

    c3t4 solution added

---
+++ sicp/chapter3/04.rkt
@@ -0,0 +1,48 @@
+#lang sicp
+
+(#%require rackunit)
+
+(define (make-account balance password)
+	(let ((incorrect-attempts 0)
+		  (max-attempts 7))
+		(define (withdraw amount)
+			(if (>= balance amount)
+				(begin (set! balance (- balance amount))
+					balance)
+				"Not enough funds!"))
+
+		(define (deposit amount)
+			(set! balance (+ balance amount))
+				balance)
+
+		(define (call-the-cops) "yo man you have problems now")
+
+	    (define (dispatch user-passwd op)
+	      (if (eq? user-passwd password)
+	          (begin
+	          	(set! incorrect-attempts 0)
+	          	(cond ((eq? op 'withdraw) withdraw)
+	                  ((eq? op 'deposit) deposit)
+	                  (else "Unknown call")))
+	          (begin
+	            (set! incorrect-attempts (+ incorrect-attempts 1))
+	            (if (>= incorrect-attempts max-attempts)
+	                (call-the-cops)
+	                "Incorrect password"))))
+	dispatch))
+
+(define incorrect-message "Incorrect password")
+
+(define acc (make-account 100 'secret-password))
+
+(check-equal? (acc 'super-secret-password 'withdraw) incorrect-message)
+(check-equal? (acc 'super-secret-password 'withdraw) incorrect-message)
+(check-equal? (acc 'super-secret-password 'withdraw) incorrect-message)
+(check-equal? ((acc 'secret-password 'withdraw) 50) 50) ; Pass correct password to test counter reset logic
+(check-equal? (acc 'super-secret-password 'withdraw) incorrect-message)
+(check-equal? (acc 'super-secret-password 'withdraw) incorrect-message)
+(check-equal? (acc 'super-secret-password 'withdraw) incorrect-message)
+(check-equal? (acc 'super-secret-password 'withdraw) incorrect-message)
+(check-equal? (acc 'super-secret-password 'withdraw) incorrect-message)
+(check-equal? (acc 'super-secret-password 'withdraw) incorrect-message)
+(check-equal? (acc 'super-secret-password 'withdraw) "yo man you have problems now")
